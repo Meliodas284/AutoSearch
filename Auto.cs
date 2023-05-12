36caeec324296c6d3d6983e161a3c39d5e63e3a1
andrey_pustovoy <ai.pustovoy284@gmail.com>
2023-05-12 11:20:28 +0700

    Добавил класс модели данных,которую буду получать из бд.

---
+++ Auto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSearch
+{
+    internal class Auto
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public string Category { get; set; }
+        public string EngineType { get; set; }
+        public int Powers { get; set; }
+        public string Transmission { get; set; }
+        public string GearType { get; set; }
+        public string Wheel { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }
+    }
+}

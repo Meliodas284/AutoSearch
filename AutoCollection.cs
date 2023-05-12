b9dc46215bcf1d08b7f97176c67a54cdb15bdac5
andrey_pustovoy <ai.pustovoy284@gmail.com>
2023-05-12 11:37:04 +0700

    Добавил класс, хранящий итоговую коллекция машин.

---
+++ AutoCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSearch
+{
+    internal class AutoCollection
+    {
+        public ObservableCollection<Auto> Cars { get; }
+        
+        public void Add(Auto auto)
+        {
+            Cars.Add(auto);
+        }
+    }
+}
